7cc59f20bdb4066f8e92c815de7784120946c3c8
Athar M <93383101+KingPin-1@users.noreply.github.com>
2023-02-03 00:12:26 +0530

    Completed test branch and project

--- webScrapperEmailSendAutomate.py
+++ webScrapperEmailSendAutomate.py
@@ -17,7 +17,7 @@ def extract(url):
     content = response.content
     soup = BeautifulSoup(content,'html.parser') # parse html in content and store in soup
     for i,tag in enumerate(soup.find_all('td',attrs={'class':"title",'valign':''})): 
-        res = "{} :: <h3>{}</h3> \n <br>".format(i+1,tag.text) if tag.text != 'More' else ''
+        res = "{} :: {} \n <br>".format(i+1,tag.text) if tag.text != 'More' else ''
         strs += res
     return strs
 
@@ -33,9 +33,27 @@ SERVER = 'smtp-mail.outlook.com'
 PORT = 587
 FROM = '1901330100158@niet.co.in'
 TO = 'mohdathar1991@gmail.com'
-PASS = 'K@whiL3onard'
+PASS = 'K4whiL3onard'
 
+msg = MIMEMultipart()
 
+msg['Subject'] = "Mohd Athar -> Top stories from HN [Automated Mail]" + str(now.day) + '-' + str(now.month) + '-' + str(now.year)
+msg['From'] = FROM
+msg['To'] = TO
 
+msg.attach(MIMEText(content,'html')) # email payload done
+
+print("Initiating Server...")
+
+server = smtplib.SMTP(SERVER,PORT)
+server.set_debuglevel(1)
+server.ehlo()
+server.starttls()
+server.login(FROM,PASS)
+server.sendmail(FROM,TO,msg.as_string())
+
+print("Email Sent")
+
+server.quit()
 
 
